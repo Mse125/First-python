351d0039f96abdba8f5aee8a6d477e1eb753eb84
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-05 20:39:41 +0100

    Create 6.py

---
+++ Course/6.py
@@ -0,0 +1,15 @@
+listagosci = {
+    ('Aro', 50, 'mezczyzna'),
+    ('Roman', 43, 'mezczyzna'),
+    ('Sofia', 65, 'kobieta')
+}
+
+listagosci2 = {
+    ('Aro', 50, 'mezczyzna'),
+    ('r', 43, 'mezczyzna'),
+    ('s', 65, 'kobieta')
+}
+
+print(listagosci)
+listagosci3 = listagosci | listagosci2
+print(listagosci3)
