7b4d5e015104948ddc6b7dc5dba89c1857dc8dd8
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-05 20:39:01 +0100

    Create 3.py

---
+++ Course/3.py
@@ -0,0 +1,16 @@
+lista1 = [881, 61, 71, 841, 91]
+lista2= ["Tomasz","Zbychu"]
+
+print(len(lista1))
+
+lista1.append(100)
+print(lista1)
+
+lista1.extend([101,201,301])
+print(lista1)
+
+lista2.insert(3,"Aro")
+print(lista2)
+
+lista1.sort(reverse=True)
+print(lista1)
