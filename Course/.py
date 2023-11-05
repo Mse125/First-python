3042df7beaad7da8c268e38ebd9435c2200c4721
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-05 20:37:35 +0100

    Create 2.py

---
+++ Course/.py
@@ -0,0 +1,35 @@
+'''
+imiona = ["as", "ad", "af"]
+
+# for imie in imiona:
+    # print(imie)
+    
+imiona[1] = "adf"
+print(imiona)
+print(imiona[1])
+
+'''
+
+imiona = ["Wojtek","Lukasz","Seba","Zbychu"]
+
+'''
+if ("Tobi" in imiona):
+    print("eluwa Wojtek")
+else: 
+    print("Tobi wyjechał")
+    
+if (int(3) not in imiona):
+    print("nie ma takiej liczby")
+    
+    
+liczba = [4] + imiona
+print(liczba)
+
+'''
+imie = input("Podaj imie: ")
+imie = imie.capitalize()
+
+if imie == "Zbychu" or imie == "Seba":
+    print("masz dostęp")
+else:
+    print("brak dostępu")
