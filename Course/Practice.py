25e48623456c605ed6a2c46b7e1fd577abb44e27
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-06 22:41:03 +0100

    Create Practice.py

---
+++ Course/Practice.py
@@ -0,0 +1,39 @@
+slownik= {}
+
+while(True):
+    print("1. Pokaz slownik: ")
+    print("2. Znajdz definicje: ")
+    print("3. Dodaj definicje: ")
+    print("4. Usun definicje: ")
+    print("5. Zakoncz: ")
+    
+    wybor = input("Co chesz zrobić: ")
+    
+    if wybor == ("1"):
+        print(slownik)
+        
+    if wybor == ("2"):
+        klucz = input("Podaj szukana definicje: "),
+        if klucz in slownik:
+            print(slownik[klucz])
+        else:
+            print("Podana definicja nie istnieje")                          
+    
+    if wybor == ("3"):
+        klucz = input("Podaj definicje: "),
+        definicja = input("Podaj tresc definciji: "),
+        slownik[klucz] = definicja
+        print("Dodano definicje")
+        
+    if wybor == ("4"):
+        klucz = input("Podaj definicje do usuniecia: "),
+        if klucz in slownik:
+            del slownik[klucz]
+            print("Definicja usunieta")
+        else:
+            print("Definicja", klucz, "nie istnieje")
+            
+    if wybor == ("5"):
+        print("Program zakonczony")
+        break
+    
