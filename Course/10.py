945ef5b5fd08016b1578beb0399d35b11b640024
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-12 21:02:05 +0100

    Create 10.py

---
+++ Course/10.py
@@ -0,0 +1,15 @@
+names = {"Seba", "Luki", "Arnold", "Zenon"}
+numbers = {1,2,3,4,5,6}
+
+nameLengt = {
+    name : len(name)
+    for name in names
+}
+
+print(nameLengt)
+
+multiNumber = {
+    number : number*number
+    for number in numbers
+}
+print(multiNumber)
