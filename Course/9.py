ffa604bde86c7baf05e6d27feeff0320dc0a5915
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-12 21:01:51 +0100

    Create 9.py

---
+++ Course/9.py
@@ -0,0 +1,16 @@
+liczby = [1, 2, 3, 4, 5, 6]
+
+element = [element**2 
+           for element in liczby]
+print(element)
+
+parzyste = [(element % 2 == 0) for element in liczby]
+print(parzyste)
+
+parzyste2 = [element
+             for element in liczby
+             if (element % 2 == 0)
+             ]
+print(parzyste2)
+
+# 1. Co zrobic na elemencie | for element in lista | warunek oparty na elmencie
