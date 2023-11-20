0ea7900b64f0e09a92c2886ee9c0054330058fda
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-20 22:31:12 +0100

    Create 16.py

---
+++ Course/16.py
@@ -0,0 +1,38 @@
+"""
+def pole_prostokata(a, b):
+    print(a * b)
+    
+pole_prostokata(2, 5)
+
+
+def pole_prostokata(a, b):
+    return a * b
+
+# print(5 * pole_prostokata(2, 8))
+
+poleA = (5 * pole_prostokata(2, 8))
+poleB = (5 * pole_prostokata(6, 12))
+
+print("Twoje pole wynosi", poleA)
+print(poleB)
+"""
+
+def dzielenie(a, b):
+    if (b == 0):
+        return
+    
+    return a / b
+
+# print(dzielenie(10, 0))
+
+dzielnieA = dzielenie(10, 2)
+
+if (dzielnieA):
+    print("Podzielono poprawnie", dzielnieA)
+    
+else:
+    print("Cos poszlo nie tak")
+    
+    
+    
+    
