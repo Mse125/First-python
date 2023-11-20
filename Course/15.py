cb4a0dc881a82499d2c5458ef02ce1adf3576443
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-20 22:31:01 +0100

    Create 15.py

---
+++ Course/15.py
@@ -0,0 +1,13 @@
+def wiadomosc_powitalna(imie):
+    print("aha",imie,"co gadasz")
+    
+# wiadomosc_powitalna("Aro")
+# wiadomosc_powitalna("Benek")
+# wiadomosc_powitalna("Arnold")
+
+imiona = ["aro","benek","arnold"]
+
+for imie in imiona:
+    wiadomosc_powitalna(imie)
+    
+    
