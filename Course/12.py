ccdebd3a9f251116ef866969f02a36a047d53410
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-20 22:30:27 +0100

    Create 12.py

---
+++ Course/12.py
@@ -0,0 +1,16 @@
+names = {"Seba", "Luki", "Arnold", "Zenon"}
+numbers = {1,2,3,4,5,6}
+
+nameLengt = {
+    name : len(name)
+    for name in names
+    if name.startswith("S")
+}
+
+print(nameLengt)
+
+multiNumber = {
+    number : number*number
+    for number in numbers
+}
+print(multiNumber)
