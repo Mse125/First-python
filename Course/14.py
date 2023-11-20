7b2ae8050f52ade39412c69194f697aec54fd0b7
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2023-11-20 22:30:50 +0100

    Create 14.py

---
+++ Course/14.py
@@ -0,0 +1,11 @@
+numbers = [
+    number
+    for number in range(2, 471)
+    if (number % 7 == 0)
+    if (number % 5 != 0)
+]
+
+# for number in numbers:
+    # print(number)
+    
+print(numbers)
