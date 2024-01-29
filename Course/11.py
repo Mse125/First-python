229343a61f0f547fecf550d9b046e14658885bba
Sebastian Muszyński <124186264+Mse125@users.noreply.github.com>
2024-01-30 00:24:30 +0100

    Update 11.py

--- Course/11.py
+++ Course/11.py
@@ -3,7 +3,7 @@ liczby = [1, 2, 3, 4, 5, 6]
 element = [element**2 
            for element in liczby]  
 print(element)
-
+ 
 parzyste = [(element % 2 == 0) for element in liczby]
 print(parzyste) 
 
